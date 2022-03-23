cc4998c941fc108d4dbd8c06d988ed96ea395914
nkolis <nurkholis010@gmail.com>
2022-03-23 22:22:55 +0700

    create database

--- app/Cores/Config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define("USER", "root");
-define("PASSWORD", "");
-define("HOST", "127.0.0.1");
-define("DBNAME", "spk-saw");
---
+++ app/Cores/Database.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SPK\App\Cores;
+
+class Database
+{
+    private \PDO $conn;
+    static function connect(){
+        
+    }
+}
